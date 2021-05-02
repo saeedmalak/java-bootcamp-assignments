cc9183e5079dc244efec493a324a88ff28cf01df
saeedmalak <makhh2001@gmail.com>
2021-05-02 17:16:27 -0400

    Used a while loop instead and closed the scanner!

--- src/com/codercampus/GuessingGame.java
+++ src/com/codercampus/GuessingGame.java
@@ -10,8 +10,10 @@ public class GuessingGame {
 		// instantiate a new random instance using Random Class
 		Random random = new Random();
 		
-		// initialize a random variable between 1 and 100 only
+		// initialize variables
 		int randomNumberGenerator = random.nextInt(100) + 1;
+
+		int numberOfGuesses = 5;
 		
 		// instantiate a new scanner instance using Scanner Class
 		Scanner scanner = new Scanner(System.in);
@@ -21,25 +23,27 @@ public class GuessingGame {
 		int userGuess = scanner.nextInt();
 		
 		// call the gameLogic function and pass in relevant parameters 
-		gameLogic(userGuess, randomNumberGenerator, scanner);
+		gameLogic(userGuess, randomNumberGenerator, scanner, numberOfGuesses);
+		scanner.close();
 	}
 	
-	static void gameLogic (int userGuess, int randomNumberGenerator, Scanner scanner) {
+	static void gameLogic (int userGuess, int randomNumberGenerator, Scanner scanner, int numberOfGuesses) {
 		
-		for (int i = 1; i <= 5; i++) {
+		while (numberOfGuesses > 0) {
 			// check for winner, wrong guesses and wrong inputs
 			if (userGuess > 100 || userGuess < 1) {
 				System.out.println();
 				System.out.println("Your guess is not between 1 and 100, please try again");
-				i--; // don't penalize the user for invalid inputs
 			}
 			else if (userGuess > randomNumberGenerator) {
 				System.out.println();
 				System.out.println("Please pick a lower number");
+				numberOfGuesses--;
 			}
 			else if (userGuess < randomNumberGenerator) {
 				System.out.println();
 				System.out.println("Please pick a higher number");
+				numberOfGuesses--;
 			}
 			else {
 				System.out.println();
@@ -47,20 +51,17 @@ public class GuessingGame {
 				System.out.println("The number to guess was: " + randomNumberGenerator);
 				break;
 			}
+			
 			// end the game if the user used all of their guesses 
-			if (i == 5) {
+			if (numberOfGuesses <= 0) {
 				System.out.println();
 				System.out.println("You lose!");
 				System.out.println("The number to guess was: " + randomNumberGenerator);
 				break;
 			}
-			// otherwise ask the user for another guess
-			else {
-				System.out.println();
-				System.out.print("Pick a number between 1 and 100: ");
-				userGuess = scanner.nextInt(); 
-			}
+			System.out.println();
+			System.out.print("Pick a number between 1 and 100: ");
+			userGuess = scanner.nextInt(); 
 		}
 	}
-
 }
